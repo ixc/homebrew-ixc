b40c2a239a1324082e8ee72b2be2372cac88e5ce
Tai Lee <real.human@mrmachine.net>
2021-03-30 12:43:11 +1100

    Update go installation for dockerize@0.6.0 from upstream.

--- Formula/dockerize@0.6.0.rb
+++ Formula/dockerize@0.6.0.rb
@@ -2,8 +2,9 @@ class DockerizeAT060 < Formula
   desc "Utility to simplify running applications in docker containers"
   homepage "https://github.com/jwilder/dockerize"
   url "https://github.com/jwilder/dockerize.git",
-      :tag      => "v0.6.0",
-      :revision => "f1ec3af610ad5e96d6349984e93e1df0b447dbd5"
+      tag:      "v0.6.0",
+      revision: "f1ec3af610ad5e96d6349984e93e1df0b447dbd5"
+  license "MIT"
 
   keg_only :versioned_formula
 
@@ -11,15 +12,21 @@ class DockerizeAT060 < Formula
 
   def install
     ENV["GOPATH"] = buildpath
+    ENV["GO111MODULE"] = "auto"
     (buildpath/"src/github.com/jwilder/dockerize").install buildpath.children
     ENV.append_path "PATH", buildpath/"bin"
     cd "src/github.com/jwilder/dockerize" do
       system "make", "dist"
-      bin.install "dist/darwin/amd64/dockerize"
+      on_macos do
+        bin.install "dist/darwin/amd64/dockerize"
+      end
+      on_linux do
+        bin.install "dist/linux/amd64/dockerize"
+      end
     end
   end
 
   test do
-    system "dist/darwin/amd64/dockerize", "--help"
+    system "#{bin}/dockerize", "-wait", "https://www.google.com/", "-wait-retry-interval=1s", "-timeout", "5s"
   end
 end
